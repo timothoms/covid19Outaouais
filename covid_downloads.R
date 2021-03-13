36795dc76aaf3eb4815ec88cc62b8982b51f5eb8
timothoms <timo@geeky.net>
2021-03-13 09:42:32 -0500

    added variants & vaccination downloads

--- covid_downloads.R
+++ covid_downloads.R
@@ -8,7 +8,10 @@ links <- list(
     schools_sit_en = "https://www.quebec.ca/en/health/health-issues/a-z/2019-coronavirus/highlights-public-private-school-systems/",
     schools_sit_fr = "https://www.quebec.ca/sante/problemes-de-sante/a-z/coronavirus-2019/faits-saillants-covid-ecoles/",
     schools_list_en = "https://www.quebec.ca/en/health/health-issues/a-z/2019-coronavirus/list-schools-reporting-covid-19-cases/",
-    schools_list_fr = "https://www.quebec.ca/sante/problemes-de-sante/a-z/coronavirus-2019/liste-des-cas-de-covid-19-dans-les-ecoles/"
+    schools_list_fr = "https://www.quebec.ca/sante/problemes-de-sante/a-z/coronavirus-2019/liste-des-cas-de-covid-19-dans-les-ecoles/",
+    qc_variants_fr = "https://www.inspq.qc.ca/covid-19/donnees/variants",
+    qc_vaccination_en = "https://www.quebec.ca/en/health/health-issues/a-z/2019-coronavirus/situation-coronavirus-in-quebec/covid-19-vaccination-data/",
+    qc_vaccination_en = "https://www.quebec.ca/sante/problemes-de-sante/a-z/coronavirus-2019/situation-coronavirus-quebec/donnees-sur-la-vaccination-covid-19/"
 )
 path <- "~/Documents/GitHub/covid19Outaouais/websites/"
 lapply(names(links), function(x) {
