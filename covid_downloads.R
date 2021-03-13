76f30dbc1e113bcbd8799fb1861c40300312367b
timothoms <timo@geeky.net>
2021-03-13 11:13:34 -0500

    fixed error

--- covid_downloads.R
+++ covid_downloads.R
@@ -11,11 +11,11 @@ links <- list(
     schools_list_fr = "https://www.quebec.ca/sante/problemes-de-sante/a-z/coronavirus-2019/liste-des-cas-de-covid-19-dans-les-ecoles/",
     qc_variants_fr = "https://www.inspq.qc.ca/covid-19/donnees/variants",
     qc_vaccination_en = "https://www.quebec.ca/en/health/health-issues/a-z/2019-coronavirus/situation-coronavirus-in-quebec/covid-19-vaccination-data/",
-    qc_vaccination_en = "https://www.quebec.ca/sante/problemes-de-sante/a-z/coronavirus-2019/situation-coronavirus-quebec/donnees-sur-la-vaccination-covid-19/"
+    qc_vaccination_fr = "https://www.quebec.ca/sante/problemes-de-sante/a-z/coronavirus-2019/situation-coronavirus-quebec/donnees-sur-la-vaccination-covid-19/"
 )
 path <- "~/Documents/GitHub/covid19Outaouais/websites/"
 lapply(names(links), function(x) {
-    download_html(links[x], file = paste(path, x, "/", format(now(), "%Y%m%d%H%M%S"), ".html", sep = ""))
+    download_html(links[[x]], file = paste(path, x, "/", format(now(), "%Y%m%d%H%M%S"), ".html", sep = ""))
 })
 file_connection <-file(paste(path, "last_download_time.txt", sep = ""))
 writeLines(as.character(now()), file_connection)
