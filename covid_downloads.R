1958b2f08e943cec15faf0a86d2ae11d05368df9
timothoms <timo@geeky.net>
2021-03-13 08:21:56 -0500

    added school situation to downloads

--- covid_downloads.R
+++ covid_downloads.R
@@ -4,13 +4,16 @@ links <- list(
     local_sit_en = "https://cisss-outaouais.gouv.qc.ca/language/en/covid19-en/",
     local_sit_fr = "https://cisss-outaouais.gouv.qc.ca/covid-19/",
     qc_sit_en = "https://www.quebec.ca/en/health/health-issues/a-z/2019-coronavirus/situation-coronavirus-in-quebec/",
-    qc_sit_fr = "https://www.quebec.ca/sante/problemes-de-sante/a-z/coronavirus-2019/situation-coronavirus-quebec/"
+    qc_sit_fr = "https://www.quebec.ca/sante/problemes-de-sante/a-z/coronavirus-2019/situation-coronavirus-quebec/",
+    schools_sit_en = "https://www.quebec.ca/en/health/health-issues/a-z/2019-coronavirus/highlights-public-private-school-systems/",
+    schools_sit_fr = "https://www.quebec.ca/sante/problemes-de-sante/a-z/coronavirus-2019/faits-saillants-covid-ecoles/",
+    schools_list_en = "https://www.quebec.ca/en/health/health-issues/a-z/2019-coronavirus/list-schools-reporting-covid-19-cases/",
+    schools_list_fr = "https://www.quebec.ca/sante/problemes-de-sante/a-z/coronavirus-2019/liste-des-cas-de-covid-19-dans-les-ecoles/"
 )
 path <- "~/Documents/GitHub/covid19Outaouais/websites/"
-download_html(links$local_sit_fr, file = paste(path, "local_sit_fr/", format(now(), "%Y%m%d%H%M%S"), ".html", sep = ""))
-download_html(links$local_sit_en, file = paste(path, "local_sit_en/", format(now(), "%Y%m%d%H%M%S"), ".html", sep = ""))
-download_html(links$qc_sit_en, file = paste(path, "qc_sit_en/", format(now(), "%Y%m%d%H%M%S"), ".html", sep = ""))
-download_html(links$qc_sit_fr, file = paste(path, "qc_sit_fr/", format(now(), "%Y%m%d%H%M%S"), ".html", sep = ""))
+lapply(names(links), function(x) {
+    download_html(links[x], file = paste(path, x, "/", format(now(), "%Y%m%d%H%M%S"), ".html", sep = ""))
+})
 file_connection <-file(paste(path, "last_download_time.txt", sep = ""))
 writeLines(as.character(now()), file_connection)
 close(file_connection)
