1cae7d4bea2002d2d383e8913f42c72ba1a28fff
timothoms <timo@geeky.net>
2021-03-16 19:57:17 -0400

    added daily data file to automation

--- covid_auto.sh
+++ covid_auto.sh
@@ -12,7 +12,7 @@ sleep 20
 /usr/local/bin/Rscript -e 'Sys.setenv(RSTUDIO_PANDOC="/Applications/RStudio.app/Contents/MacOS/pandoc"); rmarkdown::render("README.Rmd")' >> ignore/render.log 2>&1
 sleep 20
 
-git add README.md data/covid_local.RData data/data_update_time.txt README_files/figure-gfm websites/last_download_time.txt
+git add README.md data/covid_local.RData data/covid_local_daily.RData data/data_update_time.txt README_files/figure-gfm websites/last_download_time.txt
 
 git commit --message "automatic data update"
 
