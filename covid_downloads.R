e902f19583117e0cb4b66b8e0b2ce5f16c27c749
timothoms <timo@geeky.net>
2021-03-19 22:47:30 -0400

    fixed namespace error & cleaned code

--- covid_downloads.R
+++ covid_downloads.R
@@ -1,18 +1,16 @@
-library("rvest")
-library("lubridate")
 links <- list(
-    local_sit_en = "https://cisss-outaouais.gouv.qc.ca/language/en/covid19-en/",
-    local_sit_fr = "https://cisss-outaouais.gouv.qc.ca/covid-19/",
-    qc_sit_en = "https://www.quebec.ca/en/health/health-issues/a-z/2019-coronavirus/situation-coronavirus-in-quebec/",
-    qc_sit_fr = "https://www.quebec.ca/sante/problemes-de-sante/a-z/coronavirus-2019/situation-coronavirus-quebec/",
-    schools_sit_en = "https://www.quebec.ca/en/health/health-issues/a-z/2019-coronavirus/highlights-public-private-school-systems/",
-    schools_sit_fr = "https://www.quebec.ca/sante/problemes-de-sante/a-z/coronavirus-2019/faits-saillants-covid-ecoles/",
-    qc_variants_fr = "https://www.inspq.qc.ca/covid-19/donnees/variants"
+  local_sit_en = "https://cisss-outaouais.gouv.qc.ca/language/en/covid19-en/",
+  local_sit_fr = "https://cisss-outaouais.gouv.qc.ca/covid-19/",
+  qc_sit_en = "https://www.quebec.ca/en/health/health-issues/a-z/2019-coronavirus/situation-coronavirus-in-quebec/",
+  qc_sit_fr = "https://www.quebec.ca/sante/problemes-de-sante/a-z/coronavirus-2019/situation-coronavirus-quebec/",
+  schools_sit_en = "https://www.quebec.ca/en/health/health-issues/a-z/2019-coronavirus/highlights-public-private-school-systems/",
+  schools_sit_fr = "https://www.quebec.ca/sante/problemes-de-sante/a-z/coronavirus-2019/faits-saillants-covid-ecoles/",
+  qc_variants_fr = "https://www.inspq.qc.ca/covid-19/donnees/variants"
 )
 path <- "~/Documents/GitHub/covid19Outaouais/websites/"
 lapply(names(links), function(x) {
-    download_html(links[[x]], file = paste(path, x, "/", format(lubridate::now(), "%Y%m%d%H%M%S"), ".html", sep = ""))
+  xml2::download_html(links[[x]], file = paste(path, x, "/", format(lubridate::now(), "%Y%m%d%H%M%S"), ".html", sep = ""))
 })
-file_connection <-file(paste(path, "last_download_time.txt", sep = ""))
-writeLines(as.character(now()), file_connection)
+file_connection <- file(paste(path, "last_download_time.txt", sep = ""))
+writeLines(as.character(lubridate::now()), file_connection)
 close(file_connection)
