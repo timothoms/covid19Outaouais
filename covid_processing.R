b5bbbe772861a522e5df601384736c23acdc3266
timothoms <timo@geeky.net>
2021-03-22 10:15:24 -0400

    removed unneeded links

--- covid_processing.R
+++ covid_processing.R
@@ -1,18 +1,6 @@
 library("tidyverse")
 Sys.setlocale(category = "LC_ALL", locale = "en_CA.UTF-8")
 
-links <- list(
-    local_sit_defunct = "https://cisss-outaouais.gouv.qc.ca/language/en/18907-2/",
-    local_sit_en = "https://cisss-outaouais.gouv.qc.ca/language/en/covid19-en/",
-    local_sit_fr = "https://cisss-outaouais.gouv.qc.ca/covid-19/",
-    qc_sit_en = "https://www.quebec.ca/en/health/health-issues/a-z/2019-coronavirus/situation-coronavirus-in-quebec/",
-    qc_sit_fr = "https://www.quebec.ca/sante/problemes-de-sante/a-z/coronavirus-2019/situation-coronavirus-quebec/",
-    qc_public_health = "https://www.inspq.qc.ca/covid-19/donnees",
-    qc_alert = "https://www.quebec.ca/en/health/health-issues/a-z/2019-coronavirus/progressive-regional-alert-and-intervention-system/map-of-covid-19-alert-levels-by-region/",
-    qc_schools_pdf = "https://cdn-contenu.quebec.ca/cdn-contenu/adm/min/education/publications-adm/covid-19/reseauScolaire_listeEcoles_ANG.pdf",
-    can_visual = "https://health-infobase.canada.ca/covid-19/",
-    can_data = "https://www.canada.ca/en/public-health/services/diseases/coronavirus-disease-covid-19/epidemiological-economic-research-data.html"
-)
 ParseHTMLtables <- function(path) {
     ids <- files <- dir(path)
     ids <- str_replace(ids, ".snapshot", "")
