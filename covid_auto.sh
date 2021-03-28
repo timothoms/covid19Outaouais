704c0e0ad7592f28a8a4b949f544f2474c021714
timothoms <timo@geeky.net>
2021-03-27 22:21:42 -0400

    theme change

--- covid_auto.sh
+++ covid_auto.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 cd ~/Documents/GitHub/covid19Outaouais/
 
 /usr/local/bin/Rscript covid_downloads.R
@@ -9,11 +8,11 @@ sleep 20
 rm Rplots.pdf
 sleep 20
 
-/usr/local/bin/Rscript -e 'Sys.setenv(RSTUDIO_PANDOC="/Applications/RStudio.app/Contents/MacOS/pandoc"); rmarkdown::render("README.Rmd")' >> ignore/render.log 2>&1
+/usr/local/bin/Rscript -e 'Sys.setenv(RSTUDIO_PANDOC="/Applications/RStudio.app/Contents/MacOS/pandoc"); rmarkdown::render("README.Rmd", encoding = "UTF-8", output_format = "all")' >> ignore/render.log 2>&1
+mv README.html index.html
 sleep 20
 
 git add README.md data/covid_local.RData data/covid_local_daily.RData data/data_update_time.txt README_files/figure-gfm websites/last_download_time.txt
-
+git add --force index.html
 git commit --message "automatic data update"
-
 git push origin main >> ignore/push.log 2>&1
