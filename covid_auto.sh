4d90116884962f9be7283259d6e452736e9aef00
timothoms <timo@geeky.net>
2021-03-28 09:56:09 -0400

    removing figures from github repo page

--- covid_auto.sh
+++ covid_auto.sh
@@ -8,11 +8,13 @@ sleep 20
 rm Rplots.pdf
 sleep 20
 
-/usr/local/bin/Rscript -e 'Sys.setenv(RSTUDIO_PANDOC="/Applications/RStudio.app/Contents/MacOS/pandoc"); rmarkdown::render("README.Rmd", encoding = "UTF-8", output_format = "all")' >> ignore/render.log 2>&1
-mv README.html index.html
+/usr/local/bin/Rscript -e 'Sys.setenv(RSTUDIO_PANDOC="/Applications/RStudio.app/Contents/MacOS/pandoc"); rmarkdown::render("index.Rmd", encoding = "UTF-8")' >> ignore/render.log 2>&1
 sleep 20
 
-git add README.md data/covid_local.RData data/covid_local_daily.RData data/data_update_time.txt README_files/figure-gfm websites/last_download_time.txt
+# git add data/covid_local.RData
+# git add data/covid_local_daily.RData
+git add data/data_update_time.txt
+git add websites/last_download_time.txt
 git add --force index.html
-git commit --message "automatic data update"
+git commit --message "automatic site update"
 git push origin main >> ignore/push.log 2>&1
