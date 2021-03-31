dcb8558382462a1f4121ce67842c270bece84ce3
timothoms <timo@geeky.net>
2021-03-30 21:36:56 -0400

    added separate site for Toronto

--- covid_auto.sh
+++ covid_auto.sh
@@ -9,12 +9,13 @@ rm Rplots.pdf
 sleep 20
 
 /usr/local/bin/Rscript -e 'Sys.setenv(RSTUDIO_PANDOC="/Applications/RStudio.app/Contents/MacOS/pandoc"); rmarkdown::render("index.Rmd", encoding = "UTF-8")' >> ignore/render.log 2>&1
+/usr/local/bin/Rscript -e 'Sys.setenv(RSTUDIO_PANDOC="/Applications/RStudio.app/Contents/MacOS/pandoc"); rmarkdown::render("toronto.Rmd", encoding = "UTF-8")'
 sleep 20
 
 git add data/covid_local.RData
 git add data/covid_local_daily.RData
 git add data/data_update_time.txt
 git add websites/last_download_time.txt
-git add --force index.html
+git add --force index.html toronto.html
 git commit --message "automatic site update"
 git push origin main >> ignore/push.log 2>&1
