51ec9a8f99bf321553c1c85c03c195a5a6b3a98e
timothoms <timo@geeky.net>
2021-04-02 07:25:29 -0400

    got rid of absolute path

--- covid_downloads.R
+++ covid_downloads.R
@@ -1,3 +1,4 @@
+library(here)
 links <- list(
   local_sit_en = "https://cisss-outaouais.gouv.qc.ca/language/en/covid19-en/",
   local_sit_fr = "https://cisss-outaouais.gouv.qc.ca/covid-19/",
@@ -7,9 +8,9 @@ links <- list(
   schools_sit_fr = "https://www.quebec.ca/sante/problemes-de-sante/a-z/coronavirus-2019/faits-saillants-covid-ecoles/",
   qc_variants_fr = "https://www.inspq.qc.ca/covid-19/donnees/variants"
 )
-path <- "~/Documents/GitHub/covid19Outaouais/websites/"
 lapply(names(links), function(x) {
-  xml2::download_html(links[[x]], file = paste(path, x, "/", format(lubridate::now(), "%Y%m%d%H%M%S"), ".html", sep = ""))
+  xml2::download_html(links[[x]],
+    file = here("websites", x, paste(format(lubridate::now(), "%Y%m%d%H%M%S"), ".html", sep = "")))
 })
 file_connection <- file(paste(path, "last_download_time.txt", sep = ""))
 writeLines(as.character(lubridate::now()), file_connection)
