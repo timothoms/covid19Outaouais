16fff7d32277ff2fe6ef8f2d385adf0b27d4e444
timothoms <timo@geeky.net>
2021-04-04 21:15:15 -0400

    cleanup

--- websites/wayback_scraping.sh
+++ websites/wayback_scraping.sh
@@ -1,12 +1,23 @@
-<!-- wayback-machine-scraper -a 'https://cisss-outaouais.gouv.qc.ca/language/en/18907-2/$' 'https://cisss-outaouais.gouv.qc.ca/language/en/18907-2/' -->
+wayback-machine-scraper -a 'https://cisss-outaouais.gouv.qc.ca/language/en/18907-2/$' 'https://cisss-outaouais.gouv.qc.ca/language/en/18907-2/'
+
 wayback-machine-scraper -a 'https://www.quebec.ca/en/health/health-issues/a-z/2019-coronavirus/situation-coronavirus-in-quebec/$' 'https://www.quebec.ca/en/health/health-issues/a-z/2019-coronavirus/situation-coronavirus-in-quebec/'
+
 wayback-machine-scraper -a 'https://cisss-outaouais.gouv.qc.ca/language/en/covid19-en/$' 'https://cisss-outaouais.gouv.qc.ca/language/en/covid19-en/'
+
 wayback-machine-scraper -a 'https://cisss-outaouais.gouv.qc.ca/covid-19/$' 'https://cisss-outaouais.gouv.qc.ca/covid-19/'
+
 wayback-machine-scraper -a 'https://www.quebec.ca/sante/problemes-de-sante/a-z/coronavirus-2019/situation-coronavirus-quebec/$' 'https://www.quebec.ca/sante/problemes-de-sante/a-z/coronavirus-2019/situation-coronavirus-quebec/'
+
 wayback-machine-scraper -a 'https://www.quebec.ca/en/health/health-issues/a-z/2019-coronavirus/highlights-public-private-school-systems/$' 'https://www.quebec.ca/en/health/health-issues/a-z/2019-coronavirus/highlights-public-private-school-systems/'
+
 wayback-machine-scraper -a 'https://www.quebec.ca/sante/problemes-de-sante/a-z/coronavirus-2019/faits-saillants-covid-ecoles/$' 'https://www.quebec.ca/sante/problemes-de-sante/a-z/coronavirus-2019/faits-saillants-covid-ecoles/'
+
 wayback-machine-scraper -a 'https://www.quebec.ca/en/health/health-issues/a-z/2019-coronavirus/list-schools-reporting-covid-19-cases/$' 'https://www.quebec.ca/en/health/health-issues/a-z/2019-coronavirus/list-schools-reporting-covid-19-cases/'
+
 wayback-machine-scraper -a 'https://www.quebec.ca/sante/problemes-de-sante/a-z/coronavirus-2019/liste-des-cas-de-covid-19-dans-les-ecoles/$' 'https://www.quebec.ca/sante/problemes-de-sante/a-z/coronavirus-2019/liste-des-cas-de-covid-19-dans-les-ecoles/'
+
 wayback-machine-scraper -a 'https://www.inspq.qc.ca/covid-19/donnees/variants/$' 'https://www.inspq.qc.ca/covid-19/donnees/variants/'
+
 wayback-machine-scraper -a 'https://www.quebec.ca/en/health/health-issues/a-z/2019-coronavirus/situation-coronavirus-in-quebec/covid-19-vaccination-data/$' 'https://www.quebec.ca/en/health/health-issues/a-z/2019-coronavirus/situation-coronavirus-in-quebec/covid-19-vaccination-data/'
+
 wayback-machine-scraper -a 'https://www.quebec.ca/sante/problemes-de-sante/a-z/coronavirus-2019/situation-coronavirus-quebec/donnees-sur-la-vaccination-covid-19/$' 'https://www.quebec.ca/sante/problemes-de-sante/a-z/coronavirus-2019/situation-coronavirus-quebec/donnees-sur-la-vaccination-covid-19/'
