c9e024aae277aec92318b59ed4307f2f631b25ff
timothoms <timo@geeky.net>
2021-04-07 10:22:51 -0400

    file reorg

--- covid_auto.sh
+++ covid_auto.sh
@@ -8,7 +8,7 @@ sleep 20
 rm Rplots.pdf
 sleep 20
 
-/usr/local/bin/Rscript -e 'Sys.setenv(RSTUDIO_PANDOC="/Applications/RStudio.app/Contents/MacOS/pandoc", PATH="/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin"); rmarkdown::render("index.Rmd", encoding = "UTF-8", params = list(optimize = TRUE))' >> ignore/render.log 2>&1
+/usr/local/bin/Rscript -e 'Sys.setenv(RSTUDIO_PANDOC="/Applications/RStudio.app/Contents/MacOS/pandoc", PATH="/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin"); rmarkdown::render("index.Rmd", encoding = "UTF-8", params = list(optimize = TRUE))' >> logs/render.log 2>&1
 /usr/local/bin/Rscript -e 'Sys.setenv(RSTUDIO_PANDOC="/Applications/RStudio.app/Contents/MacOS/pandoc", PATH="/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin"); rmarkdown::render("toronto.Rmd", encoding = "UTF-8", params = list(optimize = TRUE))'
 sleep 20
 
@@ -21,6 +21,6 @@ git add data/opencovid_update_time.txt
 git add websites/last_download_time.txt
 git add --force index.html toronto.html
 git commit --message "automatic site update"
-git push origin main >> ignore/push.log 2>&1
+git push origin main >> logs/push.log 2>&1
 
 /usr/local/bin/Rscript covid_new.R
