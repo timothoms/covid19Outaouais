d5d4908b9d2c3b4ed39dedae2878487d425ef9cf
timothoms <timo@geeky.net>
2022-04-25 10:02:30 -0400

    removed last situation figures

--- covid_auto.sh
+++ covid_auto.sh
@@ -6,7 +6,7 @@ sleep 10
 sleep 10
 # /usr/local/bin/Rscript -e 'Sys.setenv(RSTUDIO_PANDOC="/Applications/RStudio.app/Contents/MacOS/pandoc", PATH="/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin"); rmarkdown::render_site(encoding = "UTF-8")' >> _logs/render.log
 git add _data/*.RData
-git add _data/*.feather
+# git add _data/*.feather
 git add _data/*.txt
 git add _websites/last_download_time.txt
 # git add --force docs/\*
